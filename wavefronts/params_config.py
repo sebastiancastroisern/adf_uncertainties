d2b597230e4d74647089d6232505f20a41e902c2
ICRR Profile <sebastian.castro.isern@gmail.com>
2026-01-30 11:30:56 +0900

    Added NJ Bfield

--- wavefronts/params_config.py
+++ wavefronts/params_config.py
@@ -22,6 +22,8 @@ h0 = 8_000.0  # m
 # Magnetic field
 # ======================
 
+# Values at the Xiaodushan Observatory, China
+
 modulus = 56.482  # uT (microtesla)
 B_inc = 61.6  # degrés
 B_dec = 0.1253  # degrés
@@ -37,6 +39,15 @@ B_vec_norm = np.array([
 ]))
 
 B_vec = B_vec_norm * modulus * 1e-6  # en T (tesla)
+
+# # For the no-noise simulation
+# B_dec = 0.
+# B_inc = np.pi/2. + 1.0609856522873529
+# # Magnetic field direction (unit) vector
+# Bvec = np.array([np.sin(B_inc)*np.cos(B_dec),np.sin(B_inc)*np.sin(B_dec),np.cos(B_inc)])
+
+# B_vec_norm = Bvec / np.linalg.norm(Bvec)
+# B_vec = B_vec_norm
 
 # ======================
 # Noise / detector
